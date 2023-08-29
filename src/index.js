ed4dc5c4b9ab749b7116b3cd7d38b7fc06d96ca9
Renata Santoso <santoso.renata@gmail.com>
2023-08-28 17:25:56 -0700

    Comment out React StrictMode

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-   <React.StrictMode>
+   // Using React.StrictMode StrictMode renders components twice (on dev but not production) 
+   // in order to detect any problems with your code and warn you about them (which can be quite useful).
+   //<React.StrictMode>
       <Provider store={store}>
          <App />
       </Provider>
-   </React.StrictMode>
+   //</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
